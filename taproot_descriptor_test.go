f3f1f413c0d6dffe58fc1a0bb777ae6c2c285ff0
tiero <3596602+tiero@users.noreply.github.com>
2024-09-01 23:40:44 +0200

    add gh action

--- taproot_descriptor_test.go
+++ taproot_descriptor_test.go
@@ -14,9 +14,9 @@ func TestParseTaprootDescriptor(t *testing.T) {
 	}{
 		{
 			name: "Basic Taproot",
-			desc: "tr(0279be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798,{pk(81e0351fc94c3ba05f8d68354ff44711b02223f2b32fb7f3ef3a99a90af7952c)})",
+			desc: "tr(79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798,{pk(81e0351fc94c3ba05f8d68354ff44711b02223f2b32fb7f3ef3a99a90af7952c)})",
 			expected: TaprootDescriptor{
-				InternalKey: Key{Hex: UnspendableKey},
+				InternalKey: Key{Hex: "79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798"},
 				ScriptTree: []LeafScript{
 					{Script: "pk(81e0351fc94c3ba05f8d68354ff44711b02223f2b32fb7f3ef3a99a90af7952c)", Weight: 1},
 				},
@@ -25,9 +25,9 @@ func TestParseTaprootDescriptor(t *testing.T) {
 		},
 		{
 			name: "VTXO",
-			desc: "tr(0279be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798,{pk(81e0351fc94c3ba05f8d68354ff44711b02223f2b32fb7f3ef3a99a90af7952c),and_v(v:pk(59bffef74a89f39715b9f6b8a83e53a60a458d45542f20e2e2f4f7dbffafc5f8),older(144))})",
+			desc: "tr(79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798,{pk(81e0351fc94c3ba05f8d68354ff44711b02223f2b32fb7f3ef3a99a90af7952c),and_v(v:pk(59bffef74a89f39715b9f6b8a83e53a60a458d45542f20e2e2f4f7dbffafc5f8),older(144))})",
 			expected: TaprootDescriptor{
-				InternalKey: Key{Hex: UnspendableKey},
+				InternalKey: Key{Hex: "79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798"},
 				ScriptTree: []LeafScript{
 					{Script: "pk(81e0351fc94c3ba05f8d68354ff44711b02223f2b32fb7f3ef3a99a90af7952c)", Weight: 1},
 					{Script: "and_v(v:pk(59bffef74a89f39715b9f6b8a83e53a60a458d45542f20e2e2f4f7dbffafc5f8),older(144))", Weight: 1},
@@ -37,9 +37,9 @@ func TestParseTaprootDescriptor(t *testing.T) {
 		},
 		{
 			name: "Boarding",
-			desc: "tr(0279be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798,{pk(81e0351fc94c3ba05f8d68354ff44711b02223f2b32fb7f3ef3a99a90af7952c),and_v(v:pk(59bffef74a89f39715b9f6b8a83e53a60a458d45542f20e2e2f4f7dbffafc5f8),older(52560))})",
+			desc: "tr(79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798,{pk(81e0351fc94c3ba05f8d68354ff44711b02223f2b32fb7f3ef3a99a90af7952c),and_v(v:pk(59bffef74a89f39715b9f6b8a83e53a60a458d45542f20e2e2f4f7dbffafc5f8),older(52560))})",
 			expected: TaprootDescriptor{
-				InternalKey: Key{Hex: UnspendableKey},
+				InternalKey: Key{Hex: "79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798"},
 				ScriptTree: []LeafScript{
 					{Script: "pk(81e0351fc94c3ba05f8d68354ff44711b02223f2b32fb7f3ef3a99a90af7952c)", Weight: 1},
 					{Script: "and_v(v:pk(59bffef74a89f39715b9f6b8a83e53a60a458d45542f20e2e2f4f7dbffafc5f8),older(52560))", Weight: 1},
@@ -48,16 +48,32 @@ func TestParseTaprootDescriptor(t *testing.T) {
 			wantErr: false,
 		},
 		{
-			name:     "Invalid Internal Key",
-			desc:     "tr(0379be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798,{pk(81e0351fc94c3ba05f8d68354ff44711b02223f2b32fb7f3ef3a99a90af7952c)})",
+			name:     "Invalid Key",
+			desc:     "tr(79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798G,{pk(81e0351fc94c3ba05f8d68354ff44711b02223f2b32fb7f3ef3a99a90af7952c)})",
 			expected: TaprootDescriptor{},
 			wantErr:  true,
 		},
 		{
 			name:     "Invalid Descriptor Format",
-			desc:     "tr(0279be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798)",
+			desc:     "tr(79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798)",
 			expected: TaprootDescriptor{},
 			wantErr:  true,
+		},
+
+		{
+			name:     "Invalid Descriptor Format - Missing Script Tree",
+			desc:     "tr(79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798)",
+			expected: TaprootDescriptor{},
+			wantErr:  true,
+		},
+		{
+			name: "Valid Empty Script Tree",
+			desc: "tr(79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798,{})",
+			expected: TaprootDescriptor{
+				InternalKey: Key{Hex: "79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798"},
+				ScriptTree:  []LeafScript{},
+			},
+			wantErr: false,
 		},
 	}
 
@@ -65,11 +81,11 @@ func TestParseTaprootDescriptor(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			got, err := ParseTaprootDescriptor(tt.desc)
 			if (err != nil) != tt.wantErr {
-				t.Errorf("parseTaprootDescriptor() error = %v, wantErr %v", err, tt.wantErr)
+				t.Errorf("ParseTaprootDescriptor() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
 			if !reflect.DeepEqual(got, tt.expected) {
-				t.Errorf("parseTaprootDescriptor() = %v, want %v", got, tt.expected)
+				t.Errorf("ParseTaprootDescriptor() = %v, want %v", got, tt.expected)
 			}
 		})
 	}
